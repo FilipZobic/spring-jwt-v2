f736f0bd54820214e4effa7694609dc581934ea2
Filip Zobic <zobicfilip919@gmail.com>
2022-05-24 20:32:20 +0200

    Update method signature & encrypt password

--- src/main/java/com/zobicfilip/springjwtv2/service/AuthService.java
+++ src/main/java/com/zobicfilip/springjwtv2/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.zobicfilip.springjwtv2.service;
 
+import com.zobicfilip.springjwtv2.dto.AuthLoginDTO;
 import com.zobicfilip.springjwtv2.dto.AuthRegistrationDTO;
-import com.zobicfilip.springjwtv2.model.User;
+import org.apache.commons.lang3.tuple.Pair;
 
 public interface AuthService {
+    Pair<String, String> registerUser(AuthRegistrationDTO userDto);
 
-    User registerUser(AuthRegistrationDTO userDto);
+//    Pair<String, String> loginUser(AuthLoginDTO userDto);
 }
--- src/main/java/com/zobicfilip/springjwtv2/service/AuthServiceImpl.java
+++ src/main/java/com/zobicfilip/springjwtv2/service/AuthServiceImpl.java
@@ -12,10 +12,11 @@ import com.zobicfilip.springjwtv2.repository.RoleUserRepository;
 import com.zobicfilip.springjwtv2.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -27,9 +28,14 @@ public class AuthServiceImpl implements AuthService {
 
     private final RoleUserRepository roleUserRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTService jwtService;
+
     @Override
+    @SneakyThrows
     @Transactional
-    public User registerUser(AuthRegistrationDTO userDto) {
+    public Pair<String, String> registerUser(AuthRegistrationDTO userDto) {
 
         userRepository.findUserByEmailOrUsername(userDto.getEmail(), userDto.getUsername())
                 .ifPresent(usr -> { throw new RegistrationFailedException(userDto, usr, RegErrorType.ALREADY_EXISTS); });
@@ -40,22 +46,33 @@ public class AuthServiceImpl implements AuthService {
 
 //        UUID userId = UUID.randomUUID();
 
+        // TODO fix this
         User user = User.builder()
 //                .id(userId)
-//                .roles(List.of(new RoleUser(roleUserCompKey)))
+//                .roles(List.of(new RoleUser(roleUserCompKey))) THIS does not work there could be a workaround but i did not find it
                 .countryTag(userDto.getCountryTag())
                 .dateOfBirth(userDto.getDateOfBirth())
                 .email(userDto.getEmail())
                 .enabled(true)
                 .username(userDto.getUsername())
-                .password(userDto.getPassword()) // TODO encrypt password
+                .password(passwordEncoder.encode(userDto.getPassword()))
                 .build();
         user = this.userRepository.save(user);
         RoleUserCompKey roleUserCompKey = new RoleUserCompKey(userRole.getTitle(), user.getId());
         RoleUser roleUser = roleUserRepository.save(new RoleUser(roleUserCompKey));
+
+        // # 3Way TODO find a better way for it not to be null
+        /// ????
+        // # 2Way
         roleUser = roleUserRepository.save(roleUser);
         user.addRole(roleUser);
+        roleUser.setUser(user);
+        roleUser.setRole(userRole);
+        // # 1Way
+//        user.addRole(roleUser);
 //        user = this.userRepository.save(user);
-        return user;
+        return jwtService.generateRefreshAndAccessToken(user.getId(),
+                user.getRolesInStringSet(),
+                user.getUsername());
     }
 }
