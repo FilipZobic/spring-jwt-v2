5842b28eaa65bd7d1405c6d3d5ffd764f38cefbe
Filip Zobic <zobicfilip919@gmail.com>
2022-05-24 22:00:31 +0200

    Refactor dependency from service to repository

--- src/main/java/com/zobicfilip/springjwtv2/service/UserDetailServiceImpl.java
+++ src/main/java/com/zobicfilip/springjwtv2/service/UserDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zobicfilip.springjwtv2.service;
 
+import com.zobicfilip.springjwtv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -8,32 +9,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
 
-    private final UserService userService;
-
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        try {
-            com.zobicfilip.springjwtv2.model.User user = userService.getUserByEmail(username);
-            return new User(user.getUsername(),
-                    user.getPassword(),
-                    user.getEnabled(),
-                    true,
-                    true,
-                    true,
-                    user.getRolesInStringSet()
-                            .stream()
-                            .map(SimpleGrantedAuthority::new)
-                            .collect(Collectors.toSet())
-                    );
-        } catch (AccountNotFoundException exception) {
-            throw new UsernameNotFoundException(exception.getMessage());
-        }
+        com.zobicfilip.springjwtv2.model.User user = userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException(""));
+        return new User(user.getUsername(),
+                user.getPassword(),
+                user.getEnabled(),
+                true,
+                true,
+                true,
+                user.getRolesInStringSet()
+                        .stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toSet())
+                );
     }
 }
