8572d7b3f8ec8555e639140126c5e3ee78641f9e
Filip Zobic <zobicfilip919@gmail.com>
2022-07-12 14:02:30 +0200

    Remove comments

--- src/main/java/com/zobicfilip/springjwtv2/service/AuthService.java
+++ src/main/java/com/zobicfilip/springjwtv2/service/AuthService.java
@@ -5,10 +5,4 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public interface AuthService {
     Pair<String, String> registerUser(AuthSignUpDTO userDto);
-
-    // reset password
-    // checkIf disabled(its disabled if missed password more then 10 times) on login
-    // checkIf password reset in redis
-
-//    Pair<String, String> loginUser(AuthLoginDTO userDto);
 }
