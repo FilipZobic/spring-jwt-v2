5f0819386e9667683e487d3062f6303b04d72c2f
Filip Zobic <zobicfilip919@gmail.com>
2022-07-12 13:01:20 +0200

    Add custom security User impl && Fix @Transaction UserDetailsService

--- src/main/java/com/zobicfilip/springjwtv2/service/UserDetailServiceImpl.java
+++ src/main/java/com/zobicfilip/springjwtv2/service/UserDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zobicfilip.springjwtv2.service;
 
+import com.zobicfilip.springjwtv2.model.PrincipleUser;
 import com.zobicfilip.springjwtv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
@@ -17,18 +19,21 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         com.zobicfilip.springjwtv2.model.User user = userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException(""));
-        return new User(user.getUsername(),
+        return new PrincipleUser(user.getUsername(),
                 user.getPassword(),
                 user.getEnabled(),
                 true,
                 true,
                 true,
-                user.getRolesInStringSet()
+                user.getRolesAndAuthoritiesFormatted()
                         .stream()
                         .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toSet())
+                        .collect(Collectors.toSet()),
+                user.getId(),
+                user.getEmail()
                 );
     }
 }
