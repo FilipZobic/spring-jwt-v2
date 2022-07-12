f4118a86fe7786b794013ea7ec8bae9b17f29d0d
Filip Zobic <zobicfilip919@gmail.com>
2022-07-12 13:31:04 +0200

    Refactor name changes

--- src/main/java/com/zobicfilip/springjwtv2/exception/RegistrationFailedException.java
+++ src/main/java/com/zobicfilip/springjwtv2/exception/RegistrationFailedException.java
@@ -1,15 +1,15 @@
 package com.zobicfilip.springjwtv2.exception;
 
-import com.zobicfilip.springjwtv2.dto.AuthRegistrationDTO;
+import com.zobicfilip.springjwtv2.dto.AuthSignUpDTO;
 import com.zobicfilip.springjwtv2.model.User;
 import lombok.Getter;
 
 public class RegistrationFailedException extends RuntimeException{
     // could be record
-    @Getter private final AuthRegistrationDTO dto;
+    @Getter private final AuthSignUpDTO dto;
     @Getter private final User user;
     @Getter private final RegErrorType errorType;
-    public RegistrationFailedException(AuthRegistrationDTO dto, User user, RegErrorType errorType) {
+    public RegistrationFailedException(AuthSignUpDTO dto, User user, RegErrorType errorType) {
         super();
         this.dto = dto;
         this.user = user;
