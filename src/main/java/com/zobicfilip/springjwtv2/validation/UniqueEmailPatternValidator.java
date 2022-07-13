f53c358f9322c36cce2f7d63e1cce89c1710118c
Filip Zobic <zobicfilip919@gmail.com>
2022-07-13 19:14:36 +0200

    Create email validation (pattern, uniqueness)

---
+++ src/main/java/com/zobicfilip/springjwtv2/validation/UniqueEmailPatternValidator.java
@@ -0,0 +1,39 @@
+package com.zobicfilip.springjwtv2.validation;
+
+import com.zobicfilip.springjwtv2.exception.SecurityContextAuthenticationNotFoundException;
+import com.zobicfilip.springjwtv2.service.UserService;
+import com.zobicfilip.springjwtv2.util.Util;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+public class UniqueEmailPatternValidator implements ConstraintValidator<UniqueEmail, String> {
+
+
+    private boolean ignoreRuleIfSameAsSecurityContextEmail;
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) return false;
+        if (ignoreRuleIfSameAsSecurityContextEmail) {
+            try {
+                return Util.getUserDetails().getEmail().equals(value);
+            } catch (SecurityContextAuthenticationNotFoundException ignored) {}
+        }
+        try {
+            userService.findUserByEmail(value);
+            return false;
+        } catch (AccountNotFoundException e) {
+            return true;
+        }
+    }
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) {
+        this.ignoreRuleIfSameAsSecurityContextEmail = constraintAnnotation.ignoreRuleIfSameAsSecurityContextEmail();
+    }
+}
