cc3a9fd4180e1dbc76a0b340200c89eaaea30018
Filip Zobic <zobicfilip919@gmail.com>
2022-08-01 22:34:20 +0200

    Add validation & upload, download profile picture

--- src/main/java/com/zobicfilip/springjwtv2/controller/UserController.java
+++ src/main/java/com/zobicfilip/springjwtv2/controller/UserController.java
@@ -1,20 +1,24 @@
 package com.zobicfilip.springjwtv2.controller;
 
 import com.zobicfilip.springjwtv2.service.FileStorageService;
+import com.zobicfilip.springjwtv2.validation.UserExists;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
 import static com.zobicfilip.springjwtv2.service.FileStorageService.getImageDimension;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -34,8 +38,8 @@ public class UserController {
     consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> profilePictureUpload(@RequestParam("image") MultipartFile file,
-                                               @PathVariable UUID userId) {
-        log.info("Attempting profile image upload");
+                                              @UserExists @PathVariable UUID userId) {
+        log.info("Profile image received");
 
         // size check
         if (file.getSize() > maximumByteSizeConstraint) {
@@ -56,7 +60,21 @@ public class UserController {
             log.warn("Failed reading image");
             return ResponseEntity.badRequest().build();
         }
+        log.info("Image passed all checks");
         fileStorageService.saveFile(bytes, FileStorageService.ImageType.PNG.toString(), userId.toString());
         return ResponseEntity.ok().build();
+    }
+
+//    @PreAuthorize()
+    @GetMapping(path = "{userId}/image/download",
+    produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> profilePictureDownload(@UserExists @PathVariable UUID userId) {
+        try {
+            return ResponseEntity.ok(
+                    fileStorageService.getFile(userId.toString() + ".png")
+            );
+        } catch (FileNotFoundException  e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
