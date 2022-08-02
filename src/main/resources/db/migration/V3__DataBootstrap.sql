842f991198b2bb7d1b2fd9210a61b62428eaeecf
Filip Zobic <zobicfilip919@gmail.com>
2022-08-02 21:25:49 +0200

    Update DB permissions

--- src/main/resources/db/migration/V3__DataBootstrap.sql
+++ src/main/resources/db/migration/V3__DataBootstrap.sql
@@ -1,12 +1,10 @@
 INSERT INTO application_permission (title) VALUES
-   ('USER_SELF_R'),
-   ('USER_SELF_U'),
    ('USER_ALL_R'),
    ('USER_ALL_U'),
    ('USER_ALL_D'),
    ('USER_ALL_C'),
-   ('USER_ALL_T'),
-   ('USER_ALL_P_IMG'),
+   ('USER_ALL_PROF_IMG_CRUD'),
+   ('USER_ALL_SHARED_R'),
    ('USER_*'),
    ('**');
 
@@ -18,10 +16,6 @@ INSERT INTO application_role (title) VALUES
 INSERT INTO application_role_permission (role_id, permission_id) VALUES
      ('ROLE_ADMIN', '**'),
      ('ROLE_MODERATOR', 'USER_ALL_R'),
-     ('ROLE_MODERATOR', 'USER_ALL_C'),
-     ('ROLE_MODERATOR', 'USER_ALL_T'),
-     ('ROLE_MODERATOR', 'USER_SELF_R'),
-     ('ROLE_MODERATOR', 'USER_SELF_U'),
-     ('ROLE_MODERATOR', 'USER_ALL_P_IMG'),
-     ('ROLE_USER', 'USER_SELF_R'),
-     ('ROLE_USER', 'USER_SELF_U');
+     ('ROLE_MODERATOR', 'USER_ALL_SHARED_R'),
+     ('ROLE_MODERATOR', 'USER_ALL_PROF_IMG_CRUD'),
+     ('ROLE_USER', 'USER_ALL_SHARED_R');
