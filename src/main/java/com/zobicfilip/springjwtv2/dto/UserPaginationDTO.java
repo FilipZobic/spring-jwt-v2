a3b1e5d9b3e23608431dee3372aad2cb9c5e67e5
Filip Zobic <zobicfilip919@gmail.com>
2022-08-03 22:09:36 +0200

    Fix don't return null

--- src/main/java/com/zobicfilip/springjwtv2/dto/UserPaginationDTO.java
+++ src/main/java/com/zobicfilip/springjwtv2/dto/UserPaginationDTO.java
@@ -1,5 +1,6 @@
 package com.zobicfilip.springjwtv2.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -15,7 +16,7 @@ public record UserPaginationDTO (UUID id,
                                  String countryTag,
                                  LocalDate dateOfBirth,
                                  Map<String, Set<String>> rolesAndAuthorities,
-                                 UserPaginationDetailsDTO details) {
+                                 @JsonInclude(JsonInclude.Include.NON_NULL) UserPaginationDetailsDTO details) {
 
     public record UserPaginationDetailsDTO (LocalDateTime createdAt, LocalDateTime updatedAt, Boolean enabled) {}
 }
