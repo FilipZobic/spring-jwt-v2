ff970d257968dc7c1e2c7eea9d0194d8bac28c46
Filip Zobic <zobicfilip919@gmail.com>
2022-08-03 13:22:32 +0200

    Add index for user pagination query

--- src/main/resources/db/migration/V2__Indexes.sql
+++ src/main/resources/db/migration/V2__Indexes.sql
@@ -1,2 +1,3 @@
 CREATE UNIQUE INDEX application_user_unique_index_username ON application_user (username);
 CREATE UNIQUE INDEX application_user_unique_index_email ON application_user (email);
+CREATE UNIQUE INDEX application_user_unique_index_username_and_email_and_countryTag ON application_user (username, email, country_tag);
