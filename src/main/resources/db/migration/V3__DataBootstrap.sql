6d8fa8246e7eda496acc87526694e5f1396584f7
Filip Zobic <zobicfilip919@gmail.com>
2022-08-03 22:01:26 +0200

    Add toggle enabled authority & set to moderator

--- src/main/resources/db/migration/V3__DataBootstrap.sql
+++ src/main/resources/db/migration/V3__DataBootstrap.sql
@@ -4,6 +4,7 @@ INSERT INTO application_permission (title) VALUES
    ('USER_ALL_D'),
    ('USER_ALL_C'),
    ('USER_ALL_PROF_IMG_CRUD'),
+   ('USER_ALL_ENABLED_U'),
    ('USER_ALL_SHARED_R'),
    ('USER_*'),
    ('**');
@@ -15,6 +16,7 @@ INSERT INTO application_role (title) VALUES
 
 INSERT INTO application_role_permission (role_id, permission_id) VALUES
      ('ROLE_ADMIN', '**'),
+     ('ROLE_MODERATOR', 'USER_ALL_ENABLED_U'),
      ('ROLE_MODERATOR', 'USER_ALL_R'),
      ('ROLE_MODERATOR', 'USER_ALL_SHARED_R'),
      ('ROLE_MODERATOR', 'USER_ALL_PROF_IMG_CRUD'),
