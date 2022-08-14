ced0d68f0393a0260292dd0c71b66960c0eac71d
Filip Zobic <zobicfilip919@gmail.com>
2022-08-14 19:13:04 +0200

    Add file storage service test scaffold (add when switch to S3)

---
+++ src/test/java/com/zobicfilip/springjwtv2/service/FileStorageServiceSystemImplTest.java
@@ -0,0 +1,21 @@
+package com.zobicfilip.springjwtv2.service;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+class FileStorageServiceSystemImplTest {
+
+    @Test
+    void saveFile() {
+    }
+
+    @Test
+    void getFile() {
+    }
+
+    @Test
+    void deleteFile() {
+    }
+}
